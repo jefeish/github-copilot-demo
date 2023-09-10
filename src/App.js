8a1474d95de699184ba7faf0fc8e5b1ffecd2d55
Jürgen Efeish <jefeish@github.com>
2023-09-10 18:04:15 -0400

    added react calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Calculator from './components/Calculator.js'
 import React from 'react'
+import '@primer/css/dist/primer.css';
+import Calculator from './components/Calculator.js'
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>My App</h1>
+      </header>
       <div className="MainContent">
         <Calculator />
       </div>
