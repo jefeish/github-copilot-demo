3231cece6d8290692adb00bfdea7558119fdcc19
Jürgen Efeish <jefeish@github.com>
2023-09-11 18:04:31 -0400

    added code tour steps

--- src/originals/index.js
+++ src/originals/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@primer/css/dist/primer.css';
-// import '@primer/css/dist/primer-utilities.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
